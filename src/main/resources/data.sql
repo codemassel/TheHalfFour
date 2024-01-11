59436dd1bdd39dcde743a70ff41e81c0276a5739
Codemassel <Codemassel@gmail.com>
2024-01-11 13:25:08 +0100

    Hinzufügen der DATA.SQL:
    Hier werden aktuell nur Shopitems beim start der anwendung generiert

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO shopitems (id, item_Name, item_Price, description) VALUES
+(1,'Billoyacht mit Insel für Klimakleber', 70000, 'Für Klimakleber, die ein grünes Herz haben'),
+(2,'Bornheim 385 Ghetto-Bitchgrabbler', 5000000, 'Yacht ist Yacht. Wenn ihr einfach nur flexen wollt um heiße Frauen klarzumachen interessiert euch vielleicht der einzig wahre');
